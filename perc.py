b3ed9997d2865d3e2e1759e4160517ffb5fd0b5e
J. Lawton <jaylaw1@gmail.com>
2017-04-12 13:08:16 -0800

    add manager script to perc.py

--- perc.py
+++ perc.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_script import Manager
 
 
 app = Flask(__name__)
 
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -10,5 +12,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    manager.run()
