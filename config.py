af5178c119cec6d65ecfd14c49dc81aa549dd835
J. Lawton <jaylaw1@gmail.com>
2017-04-14 20:46:39 -0800

    add updated files

---
+++ config.py
@@ -0,0 +1,5 @@
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:jaylaw48@localhost:5432/dev_logware3'
+SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SECRET_KEY = '\xd3P\xca\xfe\xc5^/}\xb6\x00\xe4\x98\xe1\xab( \xa2D\xcfB\r"s\xd4'
