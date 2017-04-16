758ea6be65418e37a8c2d3b50d022c749cbfbed7
J. Lawton <jaylaw1@gmail.com>
2017-04-15 22:21:37 -0800

    Update SECRET_KEY.

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:jaylaw48@localhost:5432/dev_logware3'
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY = '\xd3P\xca\xfe\xc5^/}\xb6\x00\xe4\x98\xe1\xab( \xa2D\xcfB\r"s\xd4'
+SECRET_KEY = '\xc3\n \x07\x96IS\xd4Oq\xda\x13`\x80\x9d\xddN\xc8\x97h\xa3\xd1\x8b\x9b'
