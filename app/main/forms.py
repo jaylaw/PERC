f59f091e2dfc69756b6e62b9b64f34580cb6bf62
J. Lawton <jaylaw1@gmail.com>
2017-04-16 03:32:16 -0800

    Final commit on App Factory structure upgrade.

---
+++ app/main/forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, SelectField, DateField
+from wtforms.validators import DataRequired
+from .. import db
+from ..models import Location
+
+
+class NameForm(FlaskForm):
+    name = StringField('Username: ', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Save Credentials')
+    submit = SubmitField('Submit')
+
+
+class CriteriaForm(FlaskForm):
+    location = SelectField(label='Location', coerce=int)
+    start_date = DateField('<b>Start Date</b> e.g. 2017-03-26',
+                           validators=[DataRequired()])
+    end_date = DateField('<b>End Date</b> e.g. 2017-03-28',
+                         validators=[DataRequired()])
+    submit = SubmitField()
+
+    def pop_loc(self):
+        locations = db.session.query(Location).all()
+        loc_choices = [loc.location_name for loc in locations]
+        loc_names = list(enumerate(loc_choices))
+        self.location.choices = loc_names
---
+++ app/main/views.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from flask import render_template, session, redirect, url_for, request
+from flask_login import login_required, login_user, logout_user
+from . import main
+from .forms import NameForm, LoginForm, CriteriaForm
+from .. import db
+from ..models import User
+
+
+@main.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@main.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(login_name=form.name.data).first()
+        if user is None:
+            flash('Not Authorized.')
+        else:
+            session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('main.index'))
+    return render_template('index.html', form=form,
+                           name=session.get('name'),
+                           current_time=datetime.utcnow())
+
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(login_name=form.username.data).first()
+        if user is None or form.password.data != 'LogWare':
+            flash('Not Authorized.')
+            return redirect(url_for('main.login', **request.args))
+        login_user(user, form.remember_me.data)
+        return redirect(request.args.get('next') or url_for('main.index'))
+    return render_template('login.html', form=form)
+
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+
+@main.route('/report', methods=['GET', 'POST'])
+@login_required
+def report():
+    form = CriteriaForm()
+    form.pop_loc()
+    if request.method == 'POST' and form.validate():
+        return redirect(url_for('results', symbol=request.form['symbol'],
+                                trend1=request.form['trend1'],
+                                trend2=request.form['trend2']))
+    return render_template('report.html', form=form)
