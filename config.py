e8f7c89d74dabc44d1c361733f88fa0e63a17eb1
J. Lawton <jaylaw1@gmail.com>
2017-04-18 20:47:25 -0800

    set track modifications to false

--- config.py
+++ config.py
@@ -21,6 +21,7 @@ class DevelopmentConfig(Config):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
             'postgresql+psycopg2://postgres@localhost/dev_logware3'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestingConfig(Config):
     TESTING = True
