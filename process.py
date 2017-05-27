70141a8c789ee64a56cd7d13101249ae25dc3665
J. Lawton <jaylaw1@gmail.com>
2017-05-26 23:54:02 -0800

    20170526 - Add methods to Report class to populate report data.   - add user provided fields   - query for location field   - methods for specs field   - hours high and low

--- process.py
+++ process.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 
 class Report:
-    def __init__(self, location, temperature, humidity, temperature_tolerance,
-                 humidity_tolerance, start_date, end_date):
+    def __init__(self, location, temperature: float, humidity: float, temperature_tolerance: float,
+                 humidity_tolerance: float, start_date, end_date):
         self.location = location
         self.temperature = temperature
         self.humidity = humidity
@@ -14,6 +14,14 @@ class Report:
         self.start_date = start_date
         self.end_date = end_date
         self.df = self.get_details()
+
+    @staticmethod
+    def celsius_to_fahr(temp_celsius):
+        """Convert Fahrenheit to Celsius
+
+        Return Celsius conversion of input"""
+        temp_fahr = (temp_celsius * 1.8) + 32
+        return temp_fahr
 
     def get_location_name(self):
         names = [loc.location_name for loc in Location.query.all()]
@@ -26,7 +34,12 @@ class Report:
         report_query = Reading.query.filter_by(location_guid=location_guid). \
             filter(Reading.time_stamp.between(self.start_date,
                                               self.end_date)).statement
+
         df = pd.read_sql(report_query, db.engine)
+
+        df.loc[df['reading_type'] == 0, 'reading'] = df.reading.apply(
+            self.celsius_to_fahr)
+
         return df
 
     def get_specification(self):
@@ -40,6 +53,26 @@ class Report:
         total_hours = pd.to_datetime(self.end_date) - pd.to_datetime(
             self.start_date)
         return total_hours
+
+    def temp_hours_high(self):
+        df = self.df
+        temps = df[df['reading_type'] == 0]
+        temps = temps.set_index('time_stamp')
+        temps['duration'] = temps.index.to_series().diff().dt.seconds.div(60,
+                                                                          fill_value=0)
+        temp_hi = temps[
+            temps.reading > (float(self.temperature) + float(self.temperature_tolerance))]
+        return temp_hi.duration.sum(axis=0) / 60
+
+    def temp_hours_low(self):
+        df = self.df
+        temps = df[df['reading_type'] == 0]
+        temps = temps.set_index('time_stamp')
+        temps['duration'] = temps.index.to_series().diff().dt.seconds.div(60,
+                                                                          fill_value=0)
+        temp_lo = temps[
+            temps.reading < (float(self.temperature) - float(self.temperature_tolerance))]
+        return temp_lo.duration.sum(axis=0) / 60
 
     def generate_summary(self):
         summary = pd.DataFrame(index=None,
@@ -58,18 +91,14 @@ class Report:
 
         summary.LOCATION = [self.get_location_name()]
 
-        summary.loc[summary.LOCATION == self.get_location_name(),'SPECIFICATION'] = self.get_specification()
-
-        summary.loc[summary.LOCATION == self.get_location_name(),'START_DATE'] = pd.to_datetime(self.start_date)
-
-        summary.loc[summary.LOCATION == self.get_location_name(),'END_DATE'] = pd.to_datetime(self.end_date)
-
-        summary.loc[summary.LOCATION == self.get_location_name(),'FIRST_POINT_RECORDED'] = self.df.time_stamp.min()
-
-        summary.loc[summary.LOCATION == self.get_location_name(),'LAST_POINT_RECORDED'] = self.df.time_stamp.max()
-
-        summary.loc[summary.LOCATION == self.get_location_name(),'TOTAL_HOURS_EVALUATED'] = (summary.END_DATE - summary.START_DATE).astype('timedelta64[s]') / 3600.0
-
-        summary.loc[summary.LOCATION == self.get_location_name(),'TOTAL_HOURS_RECORDED'] = (summary.LAST_POINT_RECORDED - summary.FIRST_POINT_RECORDED).astype('timedelta64[s]') / 3600.0
+        summary.loc[summary.LOCATION == self.get_location_name(), 'SPECIFICATION'] = self.get_specification()
+        summary.loc[summary.LOCATION == self.get_location_name(), 'START_DATE'] = pd.to_datetime(self.start_date)
+        summary.loc[summary.LOCATION == self.get_location_name(), 'END_DATE'] = pd.to_datetime(self.end_date)
+        summary.loc[summary.LOCATION == self.get_location_name(), 'FIRST_POINT_RECORDED'] = self.df.time_stamp.min()
+        summary.loc[summary.LOCATION == self.get_location_name(), 'LAST_POINT_RECORDED'] = self.df.time_stamp.max()
+        summary.loc[summary.LOCATION == self.get_location_name(), 'TOTAL_HOURS_EVALUATED'] = (summary.END_DATE - summary.START_DATE).astype('timedelta64[s]') / 3600.0
+        summary.loc[summary.LOCATION == self.get_location_name(), 'TOTAL_HOURS_RECORDED'] = (summary.LAST_POINT_RECORDED - summary.FIRST_POINT_RECORDED).astype('timedelta64[s]') / 3600.0
+        summary.loc[summary.LOCATION == self.get_location_name(), 'HOURS_TEMP_HIGH'] = self.temp_hours_high()
+        summary.loc[summary.LOCATION == self.get_location_name(), 'HOURS_TEMP_LOW'] = self.temp_hours_low()
 
         return summary.to_html()
